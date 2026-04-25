e2812c525d16fa0f8b24ccaf3459ee07a4644db1
AhsanulHoque22 <ahsanulhoque22701048@gmail.com>
2026-04-25 17:03:31 +0600

    fix: missing BeakerIcon import

--- client/src/pages/LabReports.tsx
+++ client/src/pages/LabReports.tsx
@@ -19,7 +19,8 @@ import {
   ArrowsRightLeftIcon,
   ArrowRightIcon,
   Bars3Icon,
-  UserIcon
+  UserIcon,
+  BeakerIcon
 } from '@heroicons/react/24/outline';
 import jsPDF from 'jspdf';
 import { motion, AnimatePresence } from 'framer-motion';
